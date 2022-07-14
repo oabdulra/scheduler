7b54f5b6c7ffe3aeacc023eab746b192d74156be
Osama Abdul Rahmane <oabdul65@gmail.comm>
2022-07-13 21:06:28 -0400

    refactored DayListItem to use classNames

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "components/DayListItem.scss";
+import classNames from "classnames";
 
 export default function DayListItem(props) {
   const dayName = props.name;
   const spotsRemaining = props.spots;
+  const dayClass = classNames("day-list__item",{
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": spotsRemaining === 0
+  });
+
   return (
-    <li oncClick={() => props.setDay(dayName)}>
+    <li oncClick={() => props.setDay(dayName)} className={dayClass}>
       <h2 className="text--regular">{dayName}</h2> 
       <h3 className="text--light">{spotsRemaining} spots remaining</h3>
     </li>
