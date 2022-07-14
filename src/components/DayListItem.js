8e349f94193c30fb89c6b98f099100e7d67c977f
Osama Abdul Rahmane <oabdul65@gmail.comm>
2022-07-13 20:59:19 -0400

    added style sheet for daylistitem component and imported it into component file

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,13 @@
 import React from "react";
+import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
+  const dayName = props.name;
+  const spotsRemaining = props.spots;
   return (
-    <li>
-      <h2 className="text--regular">Day Name</h2> 
-      <h3 className="text--light">X spots remaining</h3>
+    <li oncClick={() => props.setDay(dayName)}>
+      <h2 className="text--regular">{dayName}</h2> 
+      <h3 className="text--light">{spotsRemaining} spots remaining</h3>
     </li>
   );
 }
