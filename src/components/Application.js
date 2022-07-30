18efe3d21eeceb4cd9cecb53673d8ea19b5e5500
Osama Abdul Rahmane <oabdul65@gmail.comm>
2022-07-29 23:36:27 -0400

    added promise for interviewers db

--- src/components/Application.js
+++ src/components/Application.js
@@ -56,6 +56,7 @@ export default function Application(props) {
   });
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
+  const interview = getInterview(state, appointment.interview);
   
   // const setDays = (days) => {
   //   setState(prev => ({ ...prev, days }));
@@ -65,11 +66,24 @@ export default function Application(props) {
   useEffect(() => {
     Promise.all([
       axios.get('http://localhost:8001/api/days'),
-      axios.get('http://localhost:8001/api/appointments')
+      axios.get('http://localhost:8001/api/appointments'),
+      axios.get('http://localhost:8001/api/interviewers')
       ]).then((all) => {
-      setState(prev => ({...prev, days:all[0].data, appointments:all[1].data}))
+      setState(prev => ({...prev, days:all[0].data, appointments:all[1].data, interviewes:all[2].data}))
     })
   }, []);
+
+  const schedule = appointments.map((appointment) => {
+  
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />
+    );
+  });
 
   const appointmentList = dailyAppointments.map(appointment => {
     return (
