30ab81fff615b5fa5477fe68273cf181d6ba08e2
Osama Abdul Rahmane <oabdul65@gmail.comm>
2022-07-30 13:18:40 -0400

    implemented useVisualMode function and automated tests to ensure function is working as intended

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -14,3 +14,18 @@ export function getAppointmentsForDay(state, day) {
   return appointmentList;
   
 };
+
+export function getInterview(state, interview) {
+
+  if (!interview) {
+    return null;
+  }
+
+  let selectedInterviewer = {
+    student: interview.student,
+    interviewer: state.interviewers[interview.interviewer]
+  }
+
+  return selectedInterviewer;
+
+};
