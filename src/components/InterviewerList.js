3c4ba254041578e697a7d488bea4a843cc3cebe3
Osama Abdul Rahmane <oabdul65@gmail.comm>
2022-08-18 22:14:38 -0400

    added propTypes to interviewer List

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
 import "./InterviewerList.scss";
+import PropTypes from 'prop-types'; 
 
 
 export default function InterviewerList(props) {
@@ -25,4 +26,8 @@ console.log("IL line 7", props);
       <ul className="interviewers__list">{interviewerList}</ul>
     </section>
   )
+};
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
 };
